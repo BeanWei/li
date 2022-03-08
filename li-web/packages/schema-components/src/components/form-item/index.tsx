79d0fd434c054eddd9efe71bd44b3d3953abb9c1
Chaoqun Wei <chaoqunwei@Chaoquns-MacBook-Pro.local>
2022-03-08 08:57:13 +0800

    chore(li-web): improve styles

--- li-web/packages/schema-components/src/components/form-item/index.tsx
+++ li-web/packages/schema-components/src/components/form-item/index.tsx
@@ -216,25 +216,21 @@ export const BaseItem: React.FC<IFormItemProps> = ({ children, ...props }) => {
 
   const renderLabelText = () => {
     const labelChildren = (
-      <div className={`${prefixCls}-form-label-content`} ref={containerRef}>
-        <span ref={contentRef}>
-          <label>
-            {asterisk && (
-              <strong className={`${prefixCls}-form-item-symbol`}>
-                <svg
-                  fill="currentColor"
-                  viewBox="0 0 1024 1024"
-                  width="1em"
-                  height="1em"
-                >
-                  <path d="M583.338667 17.066667c18.773333 0 34.133333 15.36 34.133333 34.133333v349.013333l313.344-101.888a34.133333 34.133333 0 0 1 43.008 22.016l42.154667 129.706667a34.133333 34.133333 0 0 1-21.845334 43.178667l-315.733333 102.4 208.896 287.744a34.133333 34.133333 0 0 1-7.509333 47.786666l-110.421334 80.213334a34.133333 34.133333 0 0 1-47.786666-7.509334L505.685333 706.218667 288.426667 1005.226667a34.133333 34.133333 0 0 1-47.786667 7.509333l-110.421333-80.213333a34.133333 34.133333 0 0 1-7.509334-47.786667l214.186667-295.253333L29.013333 489.813333a34.133333 34.133333 0 0 1-22.016-43.008l42.154667-129.877333a34.133333 34.133333 0 0 1 43.008-22.016l320.512 104.106667L412.672 51.2c0-18.773333 15.36-34.133333 34.133333-34.133333h136.533334z" />
-                </svg>
-              </strong>
-            )}
-            {label}
-          </label>
-        </span>
-      </div>
+      <label ref={contentRef}>
+        {asterisk && (
+          <strong className={`${prefixCls}-form-item-symbol`}>
+            <svg
+              fill="currentColor"
+              viewBox="0 0 1024 1024"
+              width="1em"
+              height="1em"
+            >
+              <path d="M583.338667 17.066667c18.773333 0 34.133333 15.36 34.133333 34.133333v349.013333l313.344-101.888a34.133333 34.133333 0 0 1 43.008 22.016l42.154667 129.706667a34.133333 34.133333 0 0 1-21.845334 43.178667l-315.733333 102.4 208.896 287.744a34.133333 34.133333 0 0 1-7.509333 47.786666l-110.421334 80.213334a34.133333 34.133333 0 0 1-47.786666-7.509334L505.685333 706.218667 288.426667 1005.226667a34.133333 34.133333 0 0 1-47.786667 7.509333l-110.421333-80.213333a34.133333 34.133333 0 0 1-7.509334-47.786667l214.186667-295.253333L29.013333 489.813333a34.133333 34.133333 0 0 1-22.016-43.008l42.154667-129.877333a34.133333 34.133333 0 0 1 43.008-22.016l320.512 104.106667L412.672 51.2c0-18.773333 15.36-34.133333 34.133333-34.133333h136.533334z" />
+            </svg>
+          </strong>
+        )}
+        {label}
+      </label>
     );
 
     if ((tooltipLayout === "text" && tooltip) || overflow) {
@@ -263,12 +259,17 @@ export const BaseItem: React.FC<IFormItemProps> = ({ children, ...props }) => {
     if (!label) return null;
     return (
       <Grid.Col
+        ref={containerRef}
         span={enableCol && !!labelCol ? labelCol : undefined}
-        className={cls(`${prefixCls}-form-label-item`, {
-          [`${prefixCls}-form-label-item-tooltip`]:
-            (tooltip && tooltipLayout === "text") || overflow,
-          [`${prefixCls}-form-label-item-flex`]: enableCol || !!!labelCol,
-        })}
+        className={cls(
+          `${prefixCls}-form-label-item`,
+          `${prefixCls}-form-label-content`,
+          {
+            [`${prefixCls}-form-label-item-tooltip`]:
+              (tooltip && tooltipLayout === "text") || overflow,
+            [`${prefixCls}-form-label-item-flex`]: enableCol || !!!labelCol,
+          }
+        )}
         style={labelStyle}
       >
         {renderLabelText()}
