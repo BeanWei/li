4580bbdbcaa111f6ee186c604adb188a0270ca2c
Chaoqun Wei <chaoqunwei@Chaoquns-MacBook-Pro.local>
2022-03-10 21:13:29 +0800

    li-engine: wip block

---
+++ li-engine/view/block/block_listcard.go
@@ -0,0 +1,30 @@
+package block
+
+import (
+	"github.com/BeanWei/li/li-engine/view/data"
+	"github.com/BeanWei/li/li-engine/view/ui"
+)
+
+func ListCard(name string) *listcardBuilder {
+	return &listcardBuilder{schema: &ui.Schema{
+		Name:            name,
+		Type:            ui.SchemaTypeVoid,
+		XComponent:      ui.ComponentListCard,
+		XComponentProps: make(map[string]interface{}),
+		Properties:      make(map[string]*ui.Schema),
+	}}
+}
+
+type listcardBuilder struct {
+	schema *ui.Schema
+}
+
+func (b *listcardBuilder) Schema() *ui.Schema {
+	return b.schema
+}
+
+func (b *listcardBuilder) DataProvider(operation string, handler data.Handler) *listcardBuilder {
+	b.schema.XOperation = operation
+	data.RegisterHandler(operation, handler)
+	return b
+}
---
+++ li-engine/view/block/block_listtable.go
@@ -0,0 +1,30 @@
+package block
+
+import (
+	"github.com/BeanWei/li/li-engine/view/data"
+	"github.com/BeanWei/li/li-engine/view/ui"
+)
+
+func ListTable(name string) *listtableBuilder {
+	return &listtableBuilder{schema: &ui.Schema{
+		Name:            name,
+		Type:            ui.SchemaTypeVoid,
+		XComponent:      ui.ComponentListTable,
+		XComponentProps: make(map[string]interface{}),
+		Properties:      make(map[string]*ui.Schema),
+	}}
+}
+
+type listtableBuilder struct {
+	schema *ui.Schema
+}
+
+func (b *listtableBuilder) Schema() *ui.Schema {
+	return b.schema
+}
+
+func (b *listtableBuilder) DataProvider(operation string, handler data.Handler) *listtableBuilder {
+	b.schema.XOperation = operation
+	data.RegisterHandler(operation, handler)
+	return b
+}
