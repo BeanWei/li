d86f793a945cab70ab931b9f3a9cc4c283ba92aa
Bean.Wei <157318439@qq.com>
2022-03-21 23:41:58 +0800

    li-engine: improve model

--- li-engine/model/model.go
+++ li-engine/model/model.go
@@ -11,11 +11,13 @@ import (
 	"github.com/BeanWei/li/li-engine/view"
 	"github.com/BeanWei/li/li-engine/view/node"
 	"github.com/gogf/gf/v2/encoding/gjson"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/text/gstr"
 )
 
 type (
 	Schema interface {
-		Type()
+		Table() string
 		Mixin() []Mixin
 		Fields() []Field
 		Edges() []Edge
@@ -57,10 +59,14 @@ func ToFormNode(schema Schema) view.Node {
 
 func ToListNode(schema Schema) view.Node {
 	var (
+		table   = schema.Table()
 		entity  = reflect.TypeOf(schema).Elem().Name()
 		fields  = make([]Field, 0)
 		columns = make([]view.Node, 0)
 	)
+	if table == "" {
+		table = gstr.CaseSnake(entity)
+	}
 	for _, mixin := range schema.Mixin() {
 		fields = append(fields, mixin.Fields()...)
 	}
@@ -75,7 +81,6 @@ func ToListNode(schema Schema) view.Node {
 				Render(field.Descriptor().View),
 		)
 	}
-
 	columns = append(
 		columns,
 		node.ListTableColumn(fmt.Sprintf("column%d", len(fields))).
@@ -86,14 +91,14 @@ func ToListNode(schema Schema) view.Node {
 					node.ListActionRecordEditDrawer("edit").
 						Children(columns...).
 						ForInit("@get"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-							return
+							return g.Model(table).Ctx(ctx).One("id", variables.Get("id").String())
 						}).
 						ForSubmit("@update"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-							return
+							return g.Model(table).Ctx(ctx).Data(variables.Map()).Where("id", variables.Get("id").String()).Update()
 						}),
 					node.ListActionRecordDelete("delete").
 						ForSubmit("@delete"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-							return
+							return g.Model(table).Ctx(ctx).Delete("id", variables.Get("id").String())
 						}),
 				),
 			),
@@ -101,7 +106,21 @@ func ToListNode(schema Schema) view.Node {
 
 	return node.List(entity).
 		ForInit("@list"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-			return
+			// TODO: 完善列表请求
+			total, err := g.Model(table).Ctx(ctx).Count()
+			if err != nil {
+				return nil, err
+			} else if total == 0 {
+				return
+			}
+			data, err := g.Model(table).Ctx(ctx).Page(0, 20).All()
+			if err != nil {
+				return nil, err
+			}
+			return g.Map{
+				"total": total,
+				"data":  data,
+			}, nil
 		}).
 		Children(
 			node.ListAction("actions").Children(
@@ -109,12 +128,12 @@ func ToListNode(schema Schema) view.Node {
 					Title("新建").
 					Children(columns...).
 					ForSubmit("@add"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-						return
+						return g.Model(table).Ctx(ctx).Insert(variables.Map())
 					}),
 				node.ListActionRowSelection("deleteMany"+entity).
 					Title("批量删除").
 					ForSubmit("@deleteMany"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-						return
+						return g.Model(table).Ctx(ctx).Delete("id IN (?)", variables.Get("ids"))
 					}).
 					AfterReload().
 					ConfirmTitle("确认删除").
