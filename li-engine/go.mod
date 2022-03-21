0e0b82dcc6403f8c646bc1733f24d5c89cfe15d6
Bean.Wei <157318439@qq.com>
2022-03-21 22:22:29 +0800

    li-engine: remove edgedb

--- li-engine/go.mod
+++ li-engine/go.mod
@@ -2,7 +2,4 @@ module github.com/BeanWei/li/li-engine
 
 go 1.16
 
-require (
-	github.com/edgedb/edgedb-go v0.9.3
-	github.com/gogf/gf/v2 v2.0.4
-)
+require github.com/gogf/gf/v2 v2.0.4
--- li-engine/model/model.go
+++ li-engine/model/model.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
-	"strings"
 
-	"github.com/BeanWei/li/li-engine/db"
 	"github.com/BeanWei/li/li-engine/model/edge"
 	"github.com/BeanWei/li/li-engine/model/field"
 	"github.com/BeanWei/li/li-engine/model/index"
@@ -43,71 +41,6 @@ type (
 	}
 )
 
-// ToDbSchema 生成模型的 esdl
-// Note: 需要注意 schemas 的顺序，关联的对象必须放在前面
-func ToDbSchema(schemas ...Schema) string {
-	var (
-		b          strings.Builder
-		linkfields = make(map[string]bool)
-	)
-	b.WriteString("module default {\n")
-	for _, schema := range schemas {
-		b.WriteString("	type " + reflect.TypeOf(schema).Elem().Name() + " {\n ")
-		// property
-		fields := make([]Field, 0)
-		for _, mixin := range schema.Mixin() {
-			fields = append(fields, mixin.Fields()...)
-		}
-		fields = append(fields, schema.Fields()...)
-		for _, field := range fields {
-			b.WriteString("		" + field.Descriptor().ToESDL())
-		}
-		// property link
-		edges := make([]Edge, 0)
-		for _, mixin := range schema.Mixin() {
-			edges = append(edges, mixin.Edges()...)
-		}
-		edges = append(edges, schema.Edges()...)
-		for _, edge := range edges {
-			desc := edge.Descriptor()
-			linkfields[desc.Name] = true
-			b.WriteString("		" + desc.ToESDL())
-		}
-		// index
-		idxs := make([]Index, 0)
-		for _, mixin := range schema.Mixin() {
-			idxs = append(idxs, mixin.Indexes()...)
-		}
-		idxs = append(idxs, schema.Indexes()...)
-		for _, idx := range idxs {
-			fl := len(idx.Descriptor().Fields)
-			idxstrs := make([]string, fl)
-			for i, f := range idx.Descriptor().Fields {
-				if _, exists := linkfields[f]; exists {
-					idxstrs[i] = "__subject__@" + f
-				} else {
-					idxstrs[i] = "." + f
-				}
-			}
-			if fl == 1 {
-				b.WriteString("\n		index on (" + strings.Join(idxstrs, ", ") + ");")
-			} else if fl > 1 {
-				b.WriteString("\n		index on ((" + strings.Join(idxstrs, ", ") + "));")
-			}
-		}
-		b.WriteString("\n	}\n")
-	}
-	b.WriteString("}")
-	/*
-		START MIGRATION TO {
-			%s
-		};
-		POPULATE MIGRATION;
-		COMMIT MIGRATION;
-	*/
-	return b.String()
-}
-
 func ToFormNode(schema Schema) view.Node {
 	nodes := make([]view.Node, 0)
 	for _, mixin := range schema.Mixin() {
@@ -124,29 +57,16 @@ func ToFormNode(schema Schema) view.Node {
 
 func ToListNode(schema Schema) view.Node {
 	var (
-		entity    = reflect.TypeOf(schema).Elem().Name()
-		fields    = make([]Field, 0)
-		columns   = make([]view.Node, 0)
-		createCmd string
-		getCmd    string
-		updateCmd string
-		deleteCmd string
+		entity  = reflect.TypeOf(schema).Elem().Name()
+		fields  = make([]Field, 0)
+		columns = make([]view.Node, 0)
 	)
 	for _, mixin := range schema.Mixin() {
 		fields = append(fields, mixin.Fields()...)
 	}
 	fields = append(fields, schema.Fields()...)
 
-	createCmd += "INSERT " + entity + " { "
-	getCmd += "SELECT " + entity + " { id"
-	updateCmd += "UPDATE " + entity + "FILTER " + entity + ".id = <uuid>$0 SET { "
-	deleteCmd += "DELETE " + entity + "FILTER " + entity + ".id = <uuid>$0"
-
 	for i, field := range fields {
-		getCmd += ", " + field.Descriptor().Name
-		updateCmd += field.Descriptor().Name + " := " + "<" + field.Descriptor().Type + fmt.Sprintf(">$%d,", i)
-		createCmd += field.Descriptor().Name + " := " + "<" + field.Descriptor().Type + fmt.Sprintf(">$%d,", i)
-
 		columns = append(
 			columns,
 			node.ListTableColumn(fmt.Sprintf("column%d", i)).
@@ -155,10 +75,6 @@ func ToListNode(schema Schema) view.Node {
 				Render(field.Descriptor().View),
 		)
 	}
-
-	createCmd += " }"
-	getCmd += " } FILTER " + entity + ".id = <uuid>$0"
-	updateCmd += " }"
 
 	columns = append(
 		columns,
@@ -170,44 +86,35 @@ func ToListNode(schema Schema) view.Node {
 					node.ListActionRecordEditDrawer("edit").
 						Children(columns...).
 						ForInit("@get"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-							return db.Exec(ctx, getCmd, variables.Get("id").String())
+							return
 						}).
 						ForSubmit("@update"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-							args := make([]interface{}, len(fields))
-							for i, field := range fields {
-								args[i] = variables.Get(field.Descriptor().Name).Interface()
-							}
-							return db.Exec(ctx, updateCmd, args...)
+							return
 						}),
 					node.ListActionRecordDelete("delete").
 						ForSubmit("@delete"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-							return db.Exec(ctx, deleteCmd, variables.Get("id").String())
+							return
 						}),
 				),
 			),
 	)
 
 	return node.List(entity).
+		ForInit("@list"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
+			return
+		}).
 		Children(
 			node.ListAction("actions").Children(
 				node.ListActionRecordEditDrawer("add"+entity).
+					Title("新建").
 					Children(columns...).
 					ForSubmit("@add"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-						args := make([]interface{}, len(fields))
-						for i, field := range fields {
-							args[i] = variables.Get(field.Descriptor().Name).Interface()
-						}
-						return db.Exec(ctx, createCmd, args...)
+						return
 					}),
 				node.ListActionRowSelection("deleteMany"+entity).
+					Title("批量删除").
 					ForSubmit("@deleteMany"+entity, func(ctx context.Context, variables *gjson.Json) (res interface{}, err error) {
-						var b strings.Builder
-						b.WriteString("DELETE " + entity + "FILTER " + entity + ".id IN {")
-						for _, id := range variables.Get("ids").Strings() {
-							b.WriteString(`"` + id + `", "`)
-						}
-						b.WriteString("")
-						return db.Exec(ctx, b.String())
+						return
 					}).
 					AfterReload().
 					ConfirmTitle("确认删除").
