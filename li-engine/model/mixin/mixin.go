26ad16abc2e53b2c49cca52faf51528066f77a46
Bean.Wei <157318439@qq.com>
2022-03-24 14:56:31 +0800

    liengine: remove model, all in ent

--- li-engine/model/mixin/mixin.go
+++
@@ -1,9 +0,0 @@
-package mixin
-
-import "github.com/BeanWei/li/li-engine/model"
-
-type Schema struct{}
-
-func (Schema) Fields() []model.Field { return nil }
-
-func (Schema) Indexes() []model.Index { return nil }
