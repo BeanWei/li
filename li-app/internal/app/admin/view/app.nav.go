90405de34431ee59f5a1ebaabf7356db6b10f309
Bean.Wei <157318439@qq.com>
2022-03-28 09:00:12 +0800

    li-app: improve Nav

--- li-app/internal/app/admin/view/app.nav.go
+++ li-app/internal/app/admin/view/app.nav.go
@@ -1,13 +1,48 @@
 package view
 
 import (
+	"github.com/BeanWei/li/li-app/internal/data/ent/user"
 	"github.com/BeanWei/li/li-engine/view"
 	"github.com/BeanWei/li/li-engine/view/com"
+	"github.com/BeanWei/li/li-engine/view/node"
 )
 
 func NavItems() []view.Node {
 	return []view.Node{
 		com.LangSwitch("navLangSwitch"),
 		com.ThemeSwitch("navThemeSwitch"),
+		node.DropdownMenu("navUser").
+			TriggerPopupStyle(map[string]interface{}{
+				"marginRight": "28px",
+			}).
+			Droplist(
+				node.DropdownMenuItem("updateProfile").
+					Children(
+						node.ActionFormDrawer("action").
+							Title("用户设置").
+							IsMenuItem().
+							InitialValues(map[string]interface{}{
+								"nickname": `{{global.currentUser.nickname}}`,
+							}).
+							Children(
+								user.NodeNickname(),
+							),
+					),
+				node.DropdownMenuItem("updatePassword").Title("修改密码"),
+				node.Divider("divider").
+					Style(map[string]interface{}{
+						"margin": "4px 0",
+					}),
+				node.DropdownMenuItem("signOut").Title("退出登录"),
+			).
+			Children(
+				node.Avatar("currentUser").
+					Size(32).
+					Src(`{{global.currentUser.avatar}}`).
+					Style(map[string]interface{}{
+						"cursor": "pointer",
+					}).
+					SetXContent(`{{global.currentUser.nickname?.[0] || "Li"}}`),
+			),
 	}
 }
