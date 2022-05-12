58d02c94f60e99e77d789ff0d7994929119af917
Bean.Wei <157318439@qq.com>
2022-05-12 08:09:48 +0800

    li-web/admin: improve scroll style

---
+++ li-web/packages/schema-components/src/components/text-editor/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LiEditor } from "pro-components";
+
+export const TextEditor: React.FC<{}> = (props) => {
+  return <LiEditor />;
+};
+
+export default TextEditor;
